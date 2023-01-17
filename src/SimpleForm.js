1ee5433aaa9c81b14a90a973d925991b4fd29eda
amol kapadi <amolakapadi@gmail.com>
2023-01-17 18:09:59 +0530

    chages

--- src/SimpleForm.js
+++ src/SimpleForm.js
@@ -1,21 +1,12 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import ChatBot from 'react-simple-chatbot';
 import Data from './Data'
 
-class Review extends Component {
 
-}
 
-Review.propTypes = {
-  steps: PropTypes.object,
-};
-
-Review.defaultProps = {
-  steps: undefined,
-};
 
 class SimpleForm extends Component {
+  
   render() {
     return (
       <ChatBot 
@@ -23,6 +14,7 @@ class SimpleForm extends Component {
         width="100%"
         height="99vh"
         steps={Data}
+        
       />
     );
   }
--- src/Successfully.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-function Successfully (){
-    <div className='container '> 
-      <h1>Team</h1>         
-    </div>
-}
-export default Successfully;
